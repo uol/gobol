9374a7d8433b70b72fa2d086c9c817486682feb0
Rafael Nojiri <peo_rnojiri@uolinc.com>
2018-05-10 21:53:21 -0300

    Adding support to filter queries

--- solar/solr_search.go
+++ solar/solr_search.go
@@ -25,6 +25,20 @@ func (ss *SolrService) buildBasicQuery(collection, query, fields string, start, 
 	return q
 }
 
+// builFilteredQuery - builds a basic query
+func (ss *SolrService) buildFilteredQuery(collection, query, fields string, start, rows int, filterQueries []string) *solr.Query {
+
+	q := ss.buildBasicQuery(collection, query, fields, start, rows)
+
+	if filterQueries != nil && len(filterQueries) > 0 {
+		for _, fq := range filterQueries {
+			q.FilterQuery(fq)
+		}
+	}
+
+	return q
+}
+
 // SimpleQuery - queries the solr
 func (ss *SolrService) SimpleQuery(collection, query, fields string, start, rows int) (*solr.SolrResult, error) {
 
@@ -34,6 +48,24 @@ func (ss *SolrService) SimpleQuery(collection, query, fields string, start, rows
 	}
 
 	q := ss.buildBasicQuery(collection, query, fields, start, rows)
+	s := si.Search(q)
+	r, err := s.Result(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+// FilteredQuery - queries the solr
+func (ss *SolrService) FilteredQuery(collection, query, fields string, start, rows int, filterQueries []string) (*solr.SolrResult, error) {
+
+	si, err := ss.getSolrInterface(collection)
+	if err != nil {
+		return nil, err
+	}
+
+	q := ss.buildFilteredQuery(collection, query, fields, start, rows, filterQueries)
 	s := si.Search(q)
 	r, err := s.Result(nil)
 	if err != nil {
