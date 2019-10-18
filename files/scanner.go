ebccb95b7649d9bb56fed9de8050b863efc8b8b1
Rafael Nojiri <peo_rnojiri@uolinc.com>
2019-10-18 18:06:56 -0300

    Adding a helper with common file operations.

---
+++ files/scanner.go
@@ -0,0 +1,102 @@
+package files
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Configuration struct with all file properties.
+// @author rnojiri
+
+// Scanner - Scans a directory and find files using regular expression
+type Scanner struct {
+	fileRegexp   *regexp.Regexp
+	minFileSize  int64
+	logger       *zap.Logger
+	filesFound   []*File
+	ignoredFiles []*File
+	errorsFound  []error
+}
+
+// NewScanner - builds a new Scanner
+func NewScanner(fileRegexp string, minFileSize int64, logger *zap.Logger) *Scanner {
+
+	lf := []zapcore.Field{
+		zap.String("package", "files"),
+		zap.String("func", "NewScanner"),
+	}
+
+	logger.Info(fmt.Sprintf("creating a new scanner using regexp '%s' and minimum file size '%d'", fileRegexp, minFileSize), lf...)
+
+	return &Scanner{
+		fileRegexp:  regexp.MustCompile(fileRegexp),
+		minFileSize: minFileSize,
+		logger:      logger,
+	}
+}
+
+var visitLogFields = []zapcore.Field{
+	zap.String("package", "files"),
+	zap.String("func", "visit"),
+}
+
+// visit - visits a file or a directory
+func (s *Scanner) visit(path string, f os.FileInfo, err error) error {
+
+	if err != nil {
+		s.errorsFound = append(s.errorsFound, err)
+		return err
+	}
+
+	if f.IsDir() {
+		s.logger.Debug("ignoring directory:"+path, visitLogFields...)
+		return nil
+	}
+
+	file := File{
+		Name:             f.Name(),
+		Path:             path,
+		Size:             f.Size(),
+		LastModification: f.ModTime(),
+	}
+
+	if s.fileRegexp.MatchString(path) {
+		s.logger.Debug("file name matches with regexp: "+path, visitLogFields...)
+
+		if file.Size < s.minFileSize {
+			s.logger.Debug(fmt.Sprintf("file does not have the minimum size: %s (%d/%d)", path, file.Size, s.minFileSize), visitLogFields...)
+			file.Ignored = true
+			s.ignoredFiles = append(s.ignoredFiles, &file)
+			return nil
+		}
+
+		file.Ignored = false
+		s.filesFound = append(s.filesFound, &file)
+		return nil
+	}
+
+	file.Ignored = true
+	s.ignoredFiles = append(s.ignoredFiles, &file)
+	return nil
+}
+
+// Scan - searches recursively for files with the same regexp pattern selected
+func (s *Scanner) Scan(directoryRoot string) (*ScanResult, error) {
+
+	s.filesFound = []*File{}
+	s.ignoredFiles = []*File{}
+	s.errorsFound = []error{}
+
+	err := filepath.Walk(directoryRoot, s.visit)
+
+	return &ScanResult{
+		Files:   s.filesFound,
+		Ignored: s.ignoredFiles,
+		Errors:  s.errorsFound,
+	}, err
+}
---
+++ files/scanner_test.go
@@ -0,0 +1,112 @@
+package files_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"stash.uol.intranet/s3-log-uploader/files"
+	"stash.uol.intranet/s3-log-uploader/structs"
+	"stash.uol.intranet/s3-log-uploader/test"
+)
+
+// Test the file scanner
+// @author rnojiri
+
+func checkFiles(t *testing.T, scanPath string, resultFiles []*structs.File, expectedFiles []string) {
+
+	m := map[string]bool{}
+
+	for _, file := range resultFiles {
+		m[file.Path] = true
+	}
+
+	for _, file := range expectedFiles {
+		assert.True(t, m[scanPath+"/"+file])
+	}
+}
+
+func testScan(t *testing.T, regexp, subpath string, minSize int64, expectedFiles []string, ignoredFiles []string) {
+
+	s := files.NewScanner(regexp, minSize, test.GetLogger(t))
+
+	scanPath := test.GetScanPathRoot(subpath)
+	r, err := s.Scan(scanPath)
+	assert.NoError(t, err)
+	assert.Len(t, r.Errors, 0)
+	assert.Len(t, r.Files, len(expectedFiles))
+	assert.Len(t, r.Ignored, len(ignoredFiles))
+
+	checkFiles(t, scanPath, r.Files, expectedFiles)
+	checkFiles(t, scanPath, r.Ignored, ignoredFiles)
+}
+
+func TestScanAllFiles(t *testing.T) {
+
+	expected := []string{
+		"gzip/gziped-large-text.log.gz",
+		"gzip/large-text.log",
+		"rootfolder/subfolder/test.log",
+		"rootfolder/subfolder/test.log.gz",
+		"common.go",
+		"rootfolder/empty.log",
+		"rootfolder/small.log",
+	}
+
+	ignored := []string{}
+
+	testScan(t, ".*", "test", 0, expected, ignored)
+}
+
+func TestScanSingleFile(t *testing.T) {
+
+	expected := []string{
+		"common.go",
+	}
+
+	ignored := []string{
+		"gzip/gziped-large-text.log.gz",
+		"gzip/large-text.log",
+		"rootfolder/subfolder/test.log",
+		"rootfolder/subfolder/test.log.gz",
+		"rootfolder/empty.log",
+		"rootfolder/small.log",
+	}
+
+	testScan(t, "\\.go$", "test", 0, expected, ignored)
+}
+
+func TestScanNoFiles(t *testing.T) {
+
+	expected := []string{}
+
+	ignored := []string{
+		"common.go",
+		"gzip/gziped-large-text.log.gz",
+		"gzip/large-text.log",
+		"rootfolder/subfolder/test.log",
+		"rootfolder/subfolder/test.log.gz",
+		"rootfolder/empty.log",
+		"rootfolder/small.log",
+	}
+
+	testScan(t, "\\.exe$", "test", 0, expected, ignored)
+}
+
+func TestScanWithMinSize(t *testing.T) {
+
+	expected := []string{
+		"gzip/gziped-large-text.log.gz",
+		"gzip/large-text.log",
+		"rootfolder/subfolder/test.log",
+		"rootfolder/subfolder/test.log.gz",
+		"common.go",
+	}
+
+	ignored := []string{
+		"rootfolder/empty.log",
+		"rootfolder/small.log",
+	}
+
+	testScan(t, ".*", "test", 3, expected, ignored)
+}
